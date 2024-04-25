ffcc51aea05d5847e831e2cb529acd2501faec89
KoustavDev <koustav.mj.official@gmail.com>
2024-04-26 00:14:03 +0530

    service and team section added

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,10 @@ export default {
         xl:"1.5rem",
         "2xl":"1.75rem",
         "3sm":"2.25rem",
-      }
+      },
+      backgroundImage: {
+        'team-bg-image': "url('/teams/team.png')"
+      },
     },
   },
   plugins: [],
