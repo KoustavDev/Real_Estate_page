92507482321222bfd0caeaed9a42581b1de34399
KoustavDev <koustav.mj.official@gmail.com>
2024-04-28 20:09:09 +0530

    project completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,9 @@ export default {
         lg:"1.25rem",
         xl:"1.5rem",
         "2xl":"1.75rem",
-        "3sm":"2.25rem",
+        "3xl":"2.25rem",
+        "4xl":"3rem",
+        "5xl":"4rem",
       },
       backgroundImage: {
         'team-bg-image': "url('/teams/team.png')"
